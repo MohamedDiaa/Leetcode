18375d789e23e9f4581b823c9368b7ef410bc3a8
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2024-12-25 22:01:30 +0100

    solve palindrome number 9

---
+++ c#/9-Palindrome-Number/9-Palindrome-Number/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Solution s = new Solution();
+Console.WriteLine(s.IsPalindrome(121));
+Console.WriteLine(s.IsPalindrome(-121));
+Console.WriteLine(s.IsPalindrome(10));
+public class Solution {
+    public bool IsPalindrome(int x)
+    {
+        string y = x.ToString();
+
+        for (int i = 0; i < y.Length / 2; i++)
+        {
+            
+            if (y[i] != y[y.Length - i - 1])
+            {
+                return false;
+            }   
+        }
+        
+        return true;
+    }
+}
