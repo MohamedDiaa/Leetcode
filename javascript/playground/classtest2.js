a9c268d6a66d37c81f55252255536563bf20933c
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-01-05 18:03:44 +0100

    examing js strings in the playground

---
+++ javascript/playground/classtest2.js
@@ -0,0 +1,20 @@
+/*
+function User(name) {
+    this.name = name
+}
+
+User.prototype.sayHi = function() {
+    return `Hi, ${this.name}`
+};
+
+let u = new User("momo","ahmed", 32);
+console.log(u.sayHi());
+*/
+
+
+// class User {
+//     constructor() {}
+//   }
+  
+//   alert(typeof User); // function
+//   User(); // Error: Class constructor User cannot be invoked without 'new'
---
+++ javascript/playground/stringtest.js
@@ -0,0 +1,23 @@
+//let h = `Hello World`;
+
+// console.log(h[0]);
+// console.log(h.at(1));
+// console.log(h.at(h.length - 1));
+// console.log(h.at(-2));
+
+// for (const char of h) {
+//     console.log(char)
+// }
+
+// const k = h.substr(1,h.length - 2);
+// console.log(k);
+
+// console.log("Interface".toLowerCase());
+// console.log("Interface".toUpperCase());
+
+let str = "Widget with id";
+
+console.log("Widget",str.indexOf("Widget"));
+console.log("widget",str.indexOf("widget"));
+console.log("id",str.indexOf("id"));
+console.log("id", str.indexOf("id", 2));
