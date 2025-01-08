0933958f7a0139f1f0393c95a62183c71da04cf4
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-01-08 18:53:59 +0100

    solve problem  2635-Apply-Transform-Over-Each-Element-in-Array in js

---
+++ javascript/playground/arrayTest.js
@@ -0,0 +1,44 @@
+// let arr = [1,2,3,4,5];
+
+// let result = arr.reduce((sum,item) => sum + item, 0);
+
+// console.log(result);
+ let alert = console.log;
+
+// let army = {
+//     minAge: 18,
+//     maxAge: 27,
+//     canJoin(user) {
+//       return user.age >= this.minAge && user.age < this.maxAge;
+//     }
+//   };
+  
+//   let users = [
+//     {age: 16},
+//     {age: 20},
+//     {age: 23},
+//     {age: 30}
+//   ];
+  
+//   // find users, for who army.canJoin returns true
+//   let soldiers = users.filter(army.canJoin, army);
+  
+//   alert(soldiers.length); // 2
+//   alert(soldiers[0].age); // 20
+//   alert(soldiers[1].age); // 23
+
+
+// let arr = [1, 2];
+
+// // create an array from: arr and [3,4]
+// alert( arr.concat([3, 4]) ); // 1,2,3,4
+
+// // create an array from: arr and [3,4] and [5,6]
+// alert( arr.concat([3, 4], [5, 6]) ); // 1,2,3,4,5,6
+
+// // create an array from: arr and [3,4], then add values 5 and 6
+// alert( arr.concat([3, 4], 5, 6) ); // 1,2,3,4,5,6
+
+// ["Bilbo", "Gandalf", "Nazgul"].forEach((item, index, array) => {
+//     alert(`${item} is at index ${index} in ${array}`);
+//   });
