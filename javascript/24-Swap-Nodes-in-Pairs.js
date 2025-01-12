f405de09060dcb492e48934a5efae0ac875f76f9
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-01-12 20:45:31 +0100

    solved problem 24 Swap Nodes in pairs in js

--- javascript/24-Swap-Nodes-in-Pairs.js
+++ javascript/24-Swap-Nodes-in-Pairs.js
@@ -9,6 +9,59 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var swapPairs = function(head) {
-    
+var swapPairs = function (head) {
+
+    if (head.next == null) { return head; }
+
+    let newHead = new ListNode(-1, head);
+    let node = newHead;
+        // [-1,1,2,3,4]
+    while (node.next != null && node.next.next != null) {
+
+        temp1 = node.next;
+        temp2 = node.next.next;
+
+        node.next = temp2;
+        temp1.next = temp2.next;
+        temp2.next = temp1;
+
+        node = temp1
+    }
+
+    return newHead.next;
 };
+
+// Input: head = [1,2,3,4]
+// Output: [2,1,4,3]
+
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val);
+    this.next = (next === undefined ? null : next);
+}
+
+Array.prototype.convertToListNode = function () {
+    var node = null;
+    for (i = this.length - 1; i >= 0; i--) {
+        node = new ListNode(this[i], node);
+    }
+    return node;
+}
+
+ListNode.prototype.log = function () {
+    let head = this;
+
+    while (head) {
+        console.log(head.val);
+        head = head.next;
+    }
+    console.log("---");
+}
+
+
+let head = [].convertToListNode();
+if(head) {
+    head.log();
+
+    let newHead = swapPairs(head);
+    newHead.log();
+}
