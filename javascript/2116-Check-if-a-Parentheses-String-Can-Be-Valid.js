5ac457d56478b96f0f2012e1e461efe4a6eca389
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-01-13 00:05:06 +0100

    progress on solving 2116-Check-if-a-Parentheses-String-Can-Be-Valid in js

--- javascript/2116-Check-if-a-Parentheses-String-Can-Be-Valid.js
+++ javascript/2116-Check-if-a-Parentheses-String-Can-Be-Valid.js
@@ -12,31 +12,58 @@ function Node(sign,prev) {
 }
 
 var canBeValid = function(s, locked) {
-  
-    let stack = [];
-    for (let i = 0; i < s.length ; i++) {
-        console.log(s[i]);
+    
+    if(s.length%2 != 0) {
+        return false;
+    }
 
-        if(s[i].sign == `(`) {
-            let p = {
-                sign: `(`,
-                index: i
+    function travere(s, locked, node) {
+        
+        console.log(s,locked);
+
+        if(s.length == 0) {
+            let result = checkValid(node);
+            if(result) {
+                return true;
             }
-            stack.push(p);
         }
-        else if(s[i].sign == `)`) {
-            let el = stack.pop();
-            if(el != `(`) {
-
-            }
+        if(locked[0] == 0) {
+             travere(s.slice(1), locked.slice(1),  new Node(`)`, node));
+            
+            travere(s.slice(1), locked.slice(1),  new Node(`(`, node));
+                    }
+        else {
+            travere(s.slice(1), locked.slice(1),  new Node(s[0], node));
         }
     }
 
-   return stack.length == 0;
+    function checkValid(node) {
+        
+        let stack = [];
+
+        while(node) {
+            if(node.sign === `)`) {
+                stack.push(`)`);
+            }
+            else if(node.sign === `(`) {
+               let element = stack.pop();
+               if(element != `)`) {
+                    return false; 
+               }
+            }
+
+            node = node.prev.prev
+        }
+
+        return stack.length == 0;
+    }
+
+   let result = travere(s,locked,null);
+
+    return false;
 };
 
 
-let s = "))()))", locked = "010100"
-
+let s ="))()))" , locked = "010100"
 let result = canBeValid(s, locked);
 console.log(result);
