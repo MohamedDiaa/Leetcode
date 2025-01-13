d8d30572023c26e73a3bb2e439e75e2bede21d56
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-01-13 01:19:00 +0100

    progress on 2116-Check-if-a-Parentheses-String-Can-Be-Valid in js

--- javascript/2116-Check-if-a-Parentheses-String-Can-Be-Valid.js
+++ javascript/2116-Check-if-a-Parentheses-String-Can-Be-Valid.js
@@ -6,57 +6,51 @@
 
 //https://leetcode.com/problems/check-if-a-parentheses-string-can-be-valid/description/
 
-var canBeValid = function(s, locked) {
-    
-function Node(sign,prev) {
-    this.sign = sign;
-    this.prev = prev;
-}
+var canBeValid = function (s, locked) {
 
-    function travere(s, locked, node) {
-        
-        if(s.length == 0) {
-            let result = checkValid(node);
-            return result;
+    function travere(s, locked, stack) {
+
+        console.log(s, locked, stack);
+        console.log("=======");
+        if (s.length == 0) {
+            return stack.length == 0;
         }
-        if(locked[0] == 0) {
-            let r1 = travere(s.slice(1), locked.slice(1),  new Node(`)`, node));
-            if(r1) return true;
 
-            return travere(s.slice(1), locked.slice(1),  new Node(`(`, node));
-              
+        let op1 = s[0];
+        let op2 = (locked[0] == 0) ? (s[0] == `)` ? `(` : `)`) : null;
+
+        return check(op1, stack) || check(op2, stack);
+
+        function check(elment, stack) {
+
+            if (elment == null) return false;
+
+            let t1 = false;
+            let t2 = false;
+
+            if (elment == `(`) {
+                let st1 = stack.slice();
+                t1 = travere(s.slice(1), locked.slice(1), st1.push(`(`));
             }
-        else {
-            return travere(s.slice(1), locked.slice(1),  new Node(s[0], node));
+            else if (s == `)`) {
+                let st1 = stack.slice();
+                let el = st1.pop();
+                if (el == s) {
+                    t2 = false;
+                } else {
+                    t2 = travere(s.slice(1), locked.slice(1), st1);
+                }
+            }
+            return t1 || t2;
         }
+
     }
 
-    function checkValid(node) {
-        
-        let stack = [];
-
-        while(node) {
-            if(node.sign == `)`) {
-                stack.push(`)`);
-            }
-            else  {
-               let element = stack.pop();
-               if(element != `)`) {
-                    return false; 
-               }
-            }
-
-            node = node.prev
-        }
-
-        return stack.length == 0;
-    }
-
-    if(s.length%2 != 0) {
+    if (s.length % 2 != 0) {
         return false
     }
 
-    return travere(s,locked,null);     
+    return travere(s, locked, []);
 };
 
 
