503a93a294b2d9783cc04b3013bf88c95b42de50
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-01-13 16:57:55 +0100

    fix 2116-Check-if-a-Parentheses-String-Can-Be-Valid.js

--- javascript/2116-Check-if-a-Parentheses-String-Can-Be-Valid.js
+++ javascript/2116-Check-if-a-Parentheses-String-Can-Be-Valid.js
@@ -6,54 +6,99 @@
 
 //https://leetcode.com/problems/check-if-a-parentheses-string-can-be-valid/description/
 
+// var canBeValid = function (s, locked) {
+
+//     function travere(s, locked, stack) {
+
+//         console.log(s, locked, stack);
+
+//         if (s.length == 0) {
+//             return stack.length == 0;
+ 
+//         } else if(stack.length > Math.ceil(s.length/2)) {
+//             return false;
+//         }
+
+
+//         let op1 = s[0];
+//         let op2 = (locked[0] == 0) ? (s[0] == `)` ? `(` : `)`) : null;
+
+//         return check(op1, stack) || check(op2, stack);
+
+//         function check(elment, stak) {
+
+//             if (elment == null) return false;
+//             let t1 = false;
+//             let t2 = false;
+
+//             if (elment == `(`) {
+//                 let st1 = stak.slice();
+//                 st1.push(`(`)
+//                 t1 = travere(s.slice(1), locked.slice(1), st1);
+//             }
+//             else if (elment == `)`) {
+//                 let st1 = stak.slice();
+//                 let el = st1.pop();
+//                 if (el == s) {
+//                     t2 = false;
+//                 } else {
+//                     t2 = travere(s.slice(1), locked.slice(1), st1);
+//                 }
+//             }
+//             return t1 || t2;
+//         }
+
+//     }
+
+//     if (s.length % 2 != 0) {
+//         return false
+//     }
+
+//     return travere(s, locked, []);
+// };
+
 var canBeValid = function (s, locked) {
+    if (s.length % 2 !== 0) return false; // Odd length strings cannot be valid
 
-    function travere(s, locked, stack) {
+    function traverse(s, locked, stack) {
+        // Stack counters
+        let openCount = 0; // Count of opening brackets
+        let flexibleCount = 0; // Count of flexible brackets (`locked[i] == 0`)
 
-        console.log(s, locked, stack);
-        console.log("=======");
-        if (s.length == 0) {
-            return stack.length == 0;
-        }
-
-        let op1 = s[0];
-        let op2 = (locked[0] == 0) ? (s[0] == `)` ? `(` : `)`) : null;
-
-        return check(op1, stack) || check(op2, stack);
-
-        function check(elment, stack) {
-
-            if (elment == null) return false;
-
-            let t1 = false;
-            let t2 = false;
-
-            if (elment == `(`) {
-                let st1 = stack.slice();
-                t1 = travere(s.slice(1), locked.slice(1), st1.push(`(`));
-            }
-            else if (s == `)`) {
-                let st1 = stack.slice();
-                let el = st1.pop();
-                if (el == s) {
-                    t2 = false;
+        for (let i = 0; i < s.length; i++) {
+            if (locked[i] === '0') {
+                flexibleCount++; // Flexible bracket
+            } else if (s[i] === '(') {
+                openCount++; // Regular opening bracket
+            } else {
+                if (openCount > 0) {
+                    openCount--; // Match regular closing bracket
+                } else if (flexibleCount > 0) {
+                    flexibleCount--; // Use a flexible bracket
                 } else {
-                    t2 = travere(s.slice(1), locked.slice(1), st1);
+                    return false; // Invalid
                 }
             }
-            return t1 || t2;
         }
 
+        return openCount <= flexibleCount; // Ensure unmatched `(` can be balanced
     }
 
-    if (s.length % 2 != 0) {
-        return false
-    }
+    // Check left-to-right
+    if (!traverse(s, locked, [])) return false;
 
-    return travere(s, locked, []);
+    // Check right-to-left for reverse validation
+    let reversedS = s.split('').reverse().map(ch => (ch === '(' ? ')' : '(')).join('');
+    let reversedLocked = locked.split('').reverse().join('');
+
+    return traverse(reversedS, reversedLocked, []);
 };
 
 
-let s = "))()))", locked = "010100"
+let s = "()()", locked = "0000"
 let result = canBeValid(s, locked);
 console.log(result);
+
+
+
+
